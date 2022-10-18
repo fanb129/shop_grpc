f411f1bdfe1632874d60c0ea31b7f7796fa02e74
fanb <1916861581@qq.com>
2022-10-18 14:15:34 +0800

    add oss_web

---
+++ shop_api/oss_web/initialize/logger.go
@@ -0,0 +1,10 @@
+package initialize
+
+import "go.uber.org/zap"
+
+func InitLogger() {
+	dev := zap.NewDevelopmentConfig()
+	//dev.OutputPaths = []string{"/tmp/log/oss_web/dev.log", "stderr"}
+	logger, _ := dev.Build()
+	zap.ReplaceGlobals(logger)
+}
