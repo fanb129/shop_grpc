27ac20268e5bfe6241aabeb621377e253dbbfc10
fanb <1916861581@qq.com>
2022-10-18 14:17:08 +0800

    [update] 修改验证码长度为6

--- shop_api/user_web/api/chaptcha.go
+++ shop_api/user_web/api/chaptcha.go
@@ -10,7 +10,7 @@ import (
 var store = base64Captcha.DefaultMemStore
 
 func GetCaptcha(ctx *gin.Context) {
-	driver := base64Captcha.NewDriverDigit(80, 240, 5, 0.7, 80)
+	driver := base64Captcha.NewDriverDigit(80, 240, 6, 0.7, 80)
 	cp := base64Captcha.NewCaptcha(driver, store)
 	id, b64s, err := cp.Generate()
 	if err != nil {
