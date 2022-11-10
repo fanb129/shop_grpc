75429117e4975fe32590f419267532a687fd5b87
fanb <1916861581@qq.com>
2022-11-10 12:44:07 +0800

    [update] update go

--- shop_api/goods_web/build.sh
+++ shop_api/goods_web/build.sh
@@ -1,19 +1,17 @@
 echo "开始构建"
-#export GOPATH=$WORKSPACE/..
+export GOROOT=/usr/local/go
 export PATH=$PATH:$GOROOT/bin
+export GO111MODULE=on
+export GOPROXY=https://goproxy.cn
 
 # Print Go version
 go version
-
-export GO111MODULE=on
-export GOPROXY=https://goproxy.cn
-export ENV=local
 
 echo "current: ${USER}"
 #拷贝配置文件到target下
 mkdir -vp target/goods_web
 cp config-pro.yaml target/goods_web/config-pro.yaml
-cp -a start.sh target/
+cp start.sh target/
 
 go build -o target/goods_web_main main.go
 echo "构建结束"
