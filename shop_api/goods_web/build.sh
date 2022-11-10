42f3ffc3fddc098630dbf8a97b59b11e311cc176
fanb <1916861581@qq.com>
2022-11-10 12:36:45 +0800

    [update] update build.sh,copy start.sh to remote host

--- shop_api/goods_web/build.sh
+++ shop_api/goods_web/build.sh
@@ -1,19 +1,19 @@
 echo "开始构建"
 #export GOPATH=$WORKSPACE/..
-#export PATH=$PATH:$GOROOT/bin
-#
-## Print Go version
-#go version
-#
-#export GO111MODULE=on
-#export GOPROXY=https://goproxy.cn
-#export ENV=local
+export PATH=$PATH:$GOROOT/bin
+
+# Print Go version
+go version
+
+export GO111MODULE=on
+export GOPROXY=https://goproxy.cn
+export ENV=local
 
 echo "current: ${USER}"
 #拷贝配置文件到target下
 mkdir -vp target/goods_web
 cp config-pro.yaml target/goods_web/config-pro.yaml
-cp start.sh target/
+cp -a start.sh target/
 
 go build -o target/goods_web_main main.go
 echo "构建结束"
